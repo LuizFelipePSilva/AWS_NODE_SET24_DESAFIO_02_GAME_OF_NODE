80930dd65ae22f06f14305d6b944a85ac8dbd2d0
Rigoberto Fernandes <rigo.rfp@gmail.com>
2024-10-30 13:50:19 -0300

    feat: add cars routes

--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -2,6 +2,31 @@ import { Router, Request, Response } from 'express';
 
 const routes = Router();
 
+// CARS POST
+routes.post('/cars', async (req: Request, res: Response) => {
+  res.json({ message: 'Post Cars' });
+});
+
+// CARS GET ID
+routes.get('/cars/:id', async (req: Request, res: Response) =>{
+  res.json({ message: 'Get id Cars' });
+})
+
+// CARS GET ID
+routes.get('/cars/', async (req: Request, res: Response) =>{
+  res.json({ message: 'Get all Cars' });
+})
+
+// CARS DELETE ID
+routes.get('/cars/:id', async (req: Request, res: Response) =>{
+  res.json({ message: 'Delete id Cars' });
+})
+
+// CARS PATCH ID
+routes.patch('/cars/', async (req: Request, res: Response) =>{
+  res.json({ message: 'Edit id Cars' });
+})
+
 routes.get('/', async (req: Request, res: Response) => {
   res.json({ message: 'Olá mundo' });
 });
