0da9a23fb9c08facf1fd58ee785aa673bd5063ed
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2024-10-31 15:37:53 -0300

    add

--- src/shared/infra/typeorm/migrations/1730311459607-nCreateUser.ts
+++ src/shared/infra/typeorm/migrations/1730311459607-nCreateUser.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUser1730142890438 implements MigrationInterface {
+export class nCreateUser1730142890438 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -8,10 +8,8 @@ export class CreateUser1730142890438 implements MigrationInterface {
         columns: [
           { name: 'id', type: 'uuid', isPrimary: true },
           { name: 'fullName', type: 'varchar', isNullable: false },
-          { name: 'birthDate', type: 'date', isNullable: false },
-          { name: 'cpf', type: 'varchar', isUnique: true, isNullable: false },
           { name: 'email', type: 'varchar', isUnique: true, isNullable: false },
-          { name: 'phone', type: 'varchar', isNullable: false },
+          { name: 'password', type: 'varchar', isNullable: false },
           { name: 'createdAt', type: 'timestamp', default: 'now()' },
           { name: 'deletedAt', type: 'timestamp', isNullable: true },
         ],
