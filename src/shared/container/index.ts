280c5f639d0372425e56ed9006f91715ac37803a
Luiz Felipe <luizfelipep.s@outlook.com.br>
2024-11-01 15:22:25 -0300

    fix: nomeclature object Car, and use container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,14 +2,27 @@ import { container } from 'tsyringe';
 
 import { OrderRepository } from '@modules/orders/infra/typeorm/repositories/OrderRepository';
 import { IOrderRepository } from '@modules/orders/domain/repositories/IOrderRepository';
-
+import CarRepository  from '@modules/cars/infra/typeorm/repositories/CarRepository';
+import { ICarRepository } from '@modules/cars/domain/repositories/ICarRepository';
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
+import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository';
 
 import '@modules/users/providers';
 
 container.registerSingleton<IOrderRepository>(
   'OrdersRepository',
   OrderRepository,
-);
+);;
+
+container.registerSingleton<ICarRepository>(
+  'CarRepository',
+  CarRepository,
+);;
+
+container.registerSingleton<IUserRepository>(
+  'UserRepository',
+  UserRepository
+);;
 
 
 
