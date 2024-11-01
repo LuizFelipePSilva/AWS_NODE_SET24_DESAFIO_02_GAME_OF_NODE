6fcb36c72a1942432772754aca06e654bc20273b
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2024-11-01 10:52:18 -0300

    fix: adapting the find user by id code to prettier

--- src/modules/users/services/FindUserById.ts
+++ src/modules/users/services/FindUserById.ts
@@ -1,26 +1,25 @@
-import { inject, injectable } from "tsyringe";
-import { IUserRepository } from "../domain/repositories/IUserRepository";
-import { IRequestFindUserById } from "../domain/models/IRequestFindUserById";
-import { IUser } from "../domain/models/IUser";
-import AppError from "@shared/errors/AppError";
+import { inject, injectable } from 'tsyringe';
+import { IUserRepository } from '../domain/repositories/IUserRepository';
+import { IRequestFindUserById } from '../domain/models/IRequestFindUserById';
+import { IUser } from '../domain/models/IUser';
+import AppError from '@shared/errors/AppError';
 
 @injectable()
 class FindUserByIdService {
-    constructor(
-        @inject('UserRepository')
-        private userRepository: IUserRepository,
-    ) {}
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository
+  ) {}
 
-    
-    public async execute({id}: IRequestFindUserById): Promise<IUser> {
-        const user = await this.userRepository.findById(id);
-        
-        if(!user) {
-            throw new AppError('Usuário não encontrado');
-        }
+  public async execute({ id }: IRequestFindUserById): Promise<IUser> {
+    const user = await this.userRepository.findById(id);
 
-        return user;
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
     }
+
+    return user;
+  }
 }
 
 export default FindUserByIdService;
