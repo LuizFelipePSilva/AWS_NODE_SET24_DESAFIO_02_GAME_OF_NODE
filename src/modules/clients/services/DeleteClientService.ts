06344502779de174617adddb47cfaee6cde59872
Marcos Henrique <sena_marcos.fp@hotmail.com>
2024-11-01 21:10:40 -0300

    feat: updating new implementations

--- src/modules/clients/services/DeleteClientService.ts
+++ src/modules/clients/services/DeleteClientService.ts
@@ -2,7 +2,6 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import ClientRepository from '../infra/typeorm/repositories/ClientRepository';
 
-
 interface IRequest {
   id: string;
 }
@@ -10,15 +9,15 @@ interface IRequest {
 class DeleteClientService {
   public async execute({ id }: IRequest): Promise<void> {
     const clientsRepository = getCustomRepository(ClientRepository);
-
     const client = await clientsRepository.findById(id);
 
     if (!client) {
       throw new AppError('Client not found.');
     }
 
-    await clientsRepository.remove(client);
+    client.deletedAt = new Date();
+    await clientsRepository.save(client); 
   }
-
 }
+
 export default DeleteClientService;
--- src/modules/clients/services/ShowClientService.ts
+++ src/modules/clients/services/ShowClientService.ts
@@ -3,7 +3,6 @@ import { getCustomRepository } from 'typeorm';
 import Client from '../infra/typeorm/entities/Client';
 import ClientRepository from '../infra/typeorm/repositories/ClientRepository';
 
-
 interface IRequest {
   id: string;
 }
@@ -11,14 +10,14 @@ interface IRequest {
 class ShowClientService {
   public async execute({ id }: IRequest): Promise<Client> {
     const clientsRepository = getCustomRepository(ClientRepository);
-
     const client = await clientsRepository.findById(id);
 
     if (!client) {
       throw new AppError('Client not found.');
     }
-    
+
     return client;
   }
 }
+
 export default ShowClientService;
